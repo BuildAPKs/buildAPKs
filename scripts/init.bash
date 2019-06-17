b135d71cacef4c9599db2aad2a1b96e73b04ebec
SD Rausty <SDRausty@gmail.com>
2019-06-17 11:01:22 -0400

    modified:   scripts/init.bash

--- scripts/init.bash
+++ scripts/init.bash
@@ -43,19 +43,19 @@ then
 fi
 export DAY="$(date +%Y%m%d)"
 export NUM="$(date +%s)"
-export SRDR="${RDR:33}" # search.string: string manipulation site:www.tldp.org
+export SRDR="${RDR##*/}" # search: string manipulation site:www.tldp.org
 export JDR="$RDR/sources/$JID"
 if [[ ! -d "/storage/emulated/0/Download/builtAPKs/$JID$DAY" ]]
 then
 	(mkdir -p "/storage/emulated/0/Download/builtAPKs/$JID$DAY") || (mkdir -p "$RDR/gen/$JID$DAY")
 fi
 cd "$RDR"
-git pull 
+(git pull) || (printf "\\nCANNOT UPDATE ~/%s: Continuing...\\n" "${RDR##*/}") 
 if [[ -f .gitmodules ]]
 then
 	if grep shlibs .gitmodules 1>/dev/null
 	then
-		(git submodule update --init --recursive --remote scripts/shlibs) || (printf "\\nCANNOT UPDATE ~/buildAPKs/scripts/shlibs: Continuing...\\n") 
+		(git submodule update --init --recursive --remote scripts/shlibs) || (printf "\\nCANNOT UPDATE ~/%s/scripts/shlibs: Continuing...\\n" "${RDR##*/}") 
 	fi
 else
 	(git submodule add https://github.com/shlibs/shlibs scripts/shlibs) || (printf "\\nCANNOT ADD MODULE: Continuing...\\n")
