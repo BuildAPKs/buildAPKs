bb31fbbc8ef963ca29c6ea601ee80013d50e60c4
S D Rausty <27742457+sdrausty@users.noreply.github.com>
2019-07-03 23:54:41 -0400

    modified:   scripts/init/prep.bash

--- scripts/init/prep.bash
+++ scripts/init/prep.bash
@@ -37,13 +37,13 @@ trap _SPREPTRPQUIT_ QUIT
 
 find . -type f -name Android.kpf -exec rm -f {} \;
 find . -type f -name ant.properties -exec rm -f {} \;
-find . -type f -name "*.apk" -exec rm -f {} \;
+find . -type f -name '*.apk' -exec rm -f {} \;
 find . -type f -name build.xml -exec rm -f {} \;
 find . -type f -name .classpath -exec rm -f {} \;
 find . -type f -name default.properties -exec rm -f {} \;
 find . -type f -name gradle-wrapper.jar -exec rm -f {} \;
 find . -type f -name gradle-wrapper.properties -exec rm -f {} \;
-find . -type f -name "local.properties" -exec rm -f {} \;
+find . -type f -name 'local.properties' -exec rm -f {} \;
 find . -type f -name makefile -exec rm -f {} \;
 find . -type f -name makefile.linux_pc -exec rm -f {} \;
 find . -type f -name pom.xml -exec rm -f {} \;
