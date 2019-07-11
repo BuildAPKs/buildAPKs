28e820b05c2b6bd097a24876a205123090e8f80e
S D Rausty <27742457+sdrausty@users.noreply.github.com>
2019-07-10 20:47:41 -0400

    Update build.github.bash

--- scripts/build/build.github.bash
+++ scripts/build/build.github.bash
@@ -41,6 +41,7 @@ _AT_ () {
 		if [[ "$COMMIT" != "" ]] 
 		then
 			printf "%s\\n" "Found last commit $COMMIT:"
+touch .conf/"$NAME.${COMMIT::7}"
 			if [[ "$OAUT" != "" ]] 
 			then
 				ISAND="$(curl -u "$OAUT" -i "https://api.github.com/repos/$USER/$REPO/git/trees/$COMMIT?recursive=1")"
@@ -49,8 +50,10 @@ _AT_ () {
 			fi
 		 	if grep AndroidManifest.xml <<< $ISAND 
 			then
+echo 0 .conf/"$NAME.${COMMIT::7}"
 				_BUILDAPKS_
 			else
+echo 1  .conf/"$NAME.${COMMIT::7}"
 				printf "%s\\n" "Could not find an AndroidManifest.xml file in this Java language repository: NOT DOWNLOADING ${NAME##*/} tarball."
 			fi
 		elif [[ ! "${F1AR[@]}" =~ "${NAME##*/}" ]] # tests if directory exists
