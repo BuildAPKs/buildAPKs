76595dd44b3022e515bb4750159ff09cd1ce36cb
SDRausty <27742457+sdrausty@users.noreply.github.com>
2019-08-10 15:12:57 -0400

    modified:   conf/UNAMES 	modified:   scripts/sh/init/setup.BuildAPKs.sh

--- scripts/sh/init/setup.BuildAPKs.sh
+++ scripts/sh/init/setup.BuildAPKs.sh
@@ -1,20 +1,20 @@
 #!/bin/env sh 
 # Copyright 2019 (c) all rights reserved 
 # by BuildAPKs https://buildapks.github.io/buildAPKs/
-# Invocation : ~/buildAPKs/scripts/sh/init/setup.sh 
+# Invocation: ~/buildAPKs/scripts/sh/init/setup.BuildAPKs.sh 
 #####################################################################
 set -e
 
-STRINGA="Command \`au\` enables rollback; Available at https://github.com/sdrausty/au : Continuing..."
-STRINGC="Cannot update ~/buildAPKs prerequisites : Continuing..."
+STRINGA="Cannot update ~/buildAPKs prerequisites: Continuing..."
+STRINGB="Command \`au\` enables rollback; Available at https://github.com/sdrausty/au : Continuing..."
 printf "%s\\n" "Beginning buildAPKs setup:"
 for CMD in au pkg
 do
-       	[ ! -z "$(command -v "$CMD")" ] && ("$CMD" aapt apksigner curl dx ecj findutils git) || (printf "\\e[1;38;5;117m%s\\n" "$STRINGA") 
+       	[ ! -z "$(command -v "$CMD")" ] && ("$CMD" aapt apksigner curl dx ecj findutils git) || (printf "\\e[1;38;5;117m%s\\n" "$STRINGB") 
 done
 cd "$HOME"
-(git clone https://github.com/BuildAPKs/buildAPKs) || (printf "%s\\n\\n" "$STRINGC") 
-(git clone https://github.com/BuildAPKs/buildAPKs.entertainment) || (printf "%s\\n\\n" "$STRINGC") 
+(git clone https://github.com/BuildAPKs/buildAPKs) || (printf "%s\\n\\n" "$STRINGA") 
+(git clone https://github.com/BuildAPKs/buildAPKs.entertainment) || (printf "%s\\n\\n" "$STRINGA") 
 bash "$HOME/buildAPKs/scripts/bash/build/build.entertainment.bash"
 
 #EOF
