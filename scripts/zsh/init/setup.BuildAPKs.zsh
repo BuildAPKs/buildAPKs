a3ee0720a0213ded08da6c69e7be09ac4e09e49d
S D Rausty <27742457+SDRausty@users.noreply.github.com>
2019-09-21 04:15:18 -0400

    Update setup.BuildAPKs.zsh

--- scripts/zsh/init/setup.BuildAPKs.zsh
+++ scripts/zsh/init/setup.BuildAPKs.zsh
@@ -1,19 +1,17 @@
 #!/bin/env zsh 
 # Copyright 2019 (c) all rights reserved 
 # by BuildAPKs https://buildapks.github.io/buildAPKs/
-# Contributeur : https://github.com/SDRausty
-# Invocation : $HOME/buildAPKs/scripts/zsh/init/setup.BuildAPKs.zsh  
+# Invocation : ~/buildAPKs/scripts/zsh/init/setup.BuildAPKs.zsh  
 #####################################################################
 set -e
 
-STRING0="Command \`au\` enables rollback; Available at https://github.com/sdrausty/au : Continuing..."
-STRING1="Cannot update ~/buildAPKs prerequisites : Continuing..."
+STRING1="Command \`au\` enables rollback; Available at https://github.com/sdrausty/au : Continuing..."
+STRING2="Cannot update ~/buildAPKs prerequisites : Continuing..."
 printf "%s\\n" "Beginning buildAPKs setup:"
-[ -z "$(command -v au)" ] && ("$CMD" aapt apksigner curl dx ecj findutils git) || (printf "%s\\n" "$STRING0") 
-[ -z "$(command -v pkg)" ] && ("$CMD" aapt apksigner curl dx ecj findutils git) || (printf "%s\\n" "$STRING1") 
+[ -z "$(command -v au)" ] && ("$CMD" aapt apksigner curl dx ecj findutils git) || (printf "%s\\n" "$STRING1") 
+[ -z "$(command -v pkg)" ] && ("$CMD" aapt apksigner curl dx ecj findutils git) || (printf "%s\\n" "$STRING2") 
 cd "$HOME"
-(git clone https://github.com/BuildAPKs/buildAPKs) || (printf "%s\\n\\n" "$STRING1") 
-(git clone https://github.com/BuildAPKs/buildAPKs.entertainment) || (printf "%s\\n\\n" "$STRING1") 
+(git clone https://github.com/BuildAPKs/buildAPKs) || (printf "%s\\n\\n" "$STRING2") 
 bash "$HOME/buildAPKs/scripts/bash/build/build.entertainment.bash"
 
-#EOF
+# setup.BuildAPKs.zsh EOF
