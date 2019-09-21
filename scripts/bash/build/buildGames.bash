0be5c94635e175fadd0c98d06e4c534dd3363496
S D Rausty <27742457+SDRausty@users.noreply.github.com>
2019-09-21 01:22:51 -0400

    Create buildGames.bash

---
+++ scripts/bash/build/buildGames.bash
@@ -0,0 +1,41 @@
+#!/bin/env bash
+# Copyright 2019 (c) all rights reserved
+# by BuildAPKs https://buildapks.github.io/buildAPKs/
+#####################################################################
+set -Eeuo pipefail
+shopt -s nullglob globstar
+
+_SETRPERROR_() { # Run on script error.
+        local RV="$?"
+        printf "\\e[?25h\\e[1;7;38;5;0mbuildAPKs %s ERROR:  Signal %s received!\\e[0m\\n" "${0##*/}" "$RV"
+        exit 201
+}
+
+_SETRPEXIT_() { # Run on exit.
+        printf "\\e[?25h\\e[0m"
+        set +Eeuo pipefail
+        exit
+}
+
+_SETRPSIGNAL_() { # Run on signal.
+        local RV="$?"
+        printf "\\e[?25h\\e[1;7;38;5;0mbuildAPKs %s WARNING:  Signal %s received!\\e[0m\\n" "${0##*/}" "$RV"
+        exit 211
+}
+
+_SETRPQUIT_() { # Run on quit.
+        local RV="$?"
+        printf "\\e[?25h\\e[1;7;38;5;0mbuildAPKs %s WARNING:  Quit signal %s received!\\e[0m\\n" "${0##*/}" "$RV"
+        exit 221
+}
+
+trap '_SETRPERROR_ $LINENO $BASH_COMMAND $?' ERR
+trap _SETRPEXIT_ EXIT
+trap _SETRPSIGNAL_ HUP INT TERM
+trap _SETRPQUIT_ QUIT
+
+export JAD=github.com/SDRausty/buildAPKsGames
+export JID="gamez" # job id/name
+. "$HOME/buildAPKs/scripts/bash/init/init.bash"
+
+# buildGames.bash EOF
