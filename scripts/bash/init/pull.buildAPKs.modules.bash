ae41314f29872ea1ed9b33d270e9419ede4facfd
SDRausty <27742457+SDRausty@users.noreply.github.com>
2019-09-23 21:03:52 -0400

    modified:   scripts/bash/init/pull.buildAPKs.modules.bash

--- scripts/bash/init/pull.buildAPKs.modules.bash
+++ scripts/bash/init/pull.buildAPKs.modules.bash
@@ -7,7 +7,7 @@ set -e
 
 declare -A SBMS # Declare associative array for available submoldules. 
 RDR="$HOME/buildAPKs/"
-SBMS=([scripts/bash/shlibs]="$HOME/buildAPKs/scripts/bash/shlibs/.git" [sources/applications]="$HOME/buildAPKs/sources/applications/.git" [sources/browsers]="$HOME/buildAPKs/sources/browsers/.git" [sources/clocks]="$HOME/buildAPKs/sources/clocks/.git" [sources/compasses]="$HOME/buildAPKs/sources/compasses/.git" [sources/entertainment]="$HOME/buildAPKs/sources/entertainment/.git" [sources/flashlights]="$HOME/buildAPKs/sources/flashlights/.git" [sources/games]="$HOME/buildAPKs/sources/games/.git" [sources/live.wallpapers]="$HOME/buildAPKs/sources/live.wallpapers/.git" [sources/samples]="$HOME/buildAPKs/sources/samples/.git" [sources/top10]="$HOME/buildAPKs/sources/top10/.git" [sources/tools]="$HOME/buildAPKs/sources/tools/.git" [sources/tutorials]="$HOME/buildAPKs/sources/tutorials/.git" [sources/widgets]="$HOME/buildAPKs/sources/widgets/.git")
+SBMS=([scripts/bash/shlibs]="$HOME/buildAPKs/scripts/bash/shlibs/.git" [sources/applications]="$HOME/buildAPKs/sources/applications/.git" [sources/browsers]="$HOME/buildAPKs/sources/browsers/.git" [sources/clocks]="$HOME/buildAPKs/sources/clocks/.git" [sources/compasses]="$HOME/buildAPKs/sources/compasses/.git" [sources/entertainment]="$HOME/buildAPKs/sources/entertainment/.git" [sources/flashlights4]="$HOME/buildAPKs/sources/flashlights4/.git" [sources/games]="$HOME/buildAPKs/sources/games/.git" [sources/live.wallpapers]="$HOME/buildAPKs/sources/live.wallpapers/.git" [sources/samples]="$HOME/buildAPKs/sources/samples/.git" [sources/top10]="$HOME/buildAPKs/sources/top10/.git" [sources/tools]="$HOME/buildAPKs/sources/tools/.git" [sources/torches]="$HOME/buildAPKs/sources/torches/.git" [sources/tutorials]="$HOME/buildAPKs/sources/tutorials/.git" [sources/widgets]="$HOME/buildAPKs/sources/widgets/.git")
 
 _2GSU_() {
 	if [[ "$SBMI" = "" ]] 
@@ -45,12 +45,13 @@ _GSMU_() {
 	_GSU_ ./sources/clocks
 	_GSU_ ./sources/compasses 
 	_GSU_ ./sources/entertainment
-	_GSU_ ./sources/flashlights 
+	_GSU_ ./sources/flashlights4
 	_GSU_ ./sources/games 
 	_GSU_ ./sources/live.wallpapers
 	_GSU_ ./sources/samples 
 	_GSU_ ./sources/top10 
 	_GSU_ ./sources/tools 
+	_GSU_ ./sources/torches
 	_GSU_ ./sources/tutorials
 	_GSU_ ./sources/widgets
 }
