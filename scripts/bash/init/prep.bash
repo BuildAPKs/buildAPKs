f65203d6b9e2e143758fe3742b6fc5ace5edfc9e
SDRausty <27742457+SDRausty@users.noreply.github.com>
2019-09-28 14:06:05 -0400

    modified:   prep.bash

--- scripts/bash/init/prep.bash
+++ scripts/bash/init/prep.bash
@@ -36,6 +36,8 @@ trap _SPREPTRPSIGNAL_ HUP INT TERM
 trap _SPREPTRPQUIT_ QUIT 
 
 _AF_ () { # finds and removes superfluous files
+	printf "\\n%s\\n"
+	"Preparing directory $JDR/$1/"
 	for name in "${FLIST[@]}" 
 	do
  		find "$JDR/$1/" -type f -name "$name" -delete
