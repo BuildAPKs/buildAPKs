6e1efdcf067d502e8029ea2022e5c924893e2d77
SDRausty <27742457+SDRausty@users.noreply.github.com>
2019-09-30 17:09:21 -0400

    modified:   scripts/bash/build/build.github.what.is.it.bash

--- scripts/bash/build/build.github.what.is.it.bash
+++ scripts/bash/build/build.github.what.is.it.bash
@@ -1,13 +1,13 @@
 #!/bin/env bash
 # Copyright 2019 (c) all rights reserved by BuildAPKs see LICENSE
-# buildapks.github.io/buildAPKs/ published courtesy pages.github.com/
+# buildapks.github.io/buildAPKs published courtesy pages.github.com
 #####################################################################
 set -Eeuo pipefail
 shopt -s nullglob globstar
 
 _SITRPERROR_() { # run on script error
 	local RV="$?"
-	printf "\\n%s build.github.what.is.it.bash\\n" "$RV" 
+	printf "\\e[?25h\\e[1;7;38;5;0mBuildAPKs build.github.what.is.it.bash %s WARNING:  Error %s received!\\e[0m\\n" "${0##*/}" "$RV"
 	printf "\\e[?25h\\n\\e[1;48;5;138mBuildAPKs build.github.what.is.it.bash %s %s ERROR:  Generated script error %s near or at line number %s by \`%s\`!\\e[0m\\n" "${0##*/}" "${PWD##*/}" "${1:-UNDEF}" "${2:-LINENO}" "${3:-BASH_COMMAND}"
 	_WAKEUNLOCK_
 	exit 147
@@ -21,15 +21,15 @@ _SITRPEXIT_() { # run on exit
 
 _SITRPSIGNAL_() { # run on signal
 	local RV="$?"
-	printf "\\n%s build.github.what.is.it.bash\\n" "$RV" 
-	_WAKEUNLOCK_
 	printf "\\e[?25h\\e[1;7;38;5;0mBuildAPKs build.github.what.is.it.bash %s WARNING:  Signal %s received!\\e[0m\\n" "${0##*/}" "$RV"
+        printf "\\e[?25h\\n\\e[1;48;5;138mBuildAPKs build.github.what.is.it.bash %s WARNING:  Signal %s received near or at line number %s by \`%s\`!\\e[0m\\n" "${0##*/}" "${1:-UNDEFINED}" "${2:-LINENO}" "${3:-BASH_COMMAND}"
+	_WAKEUNLOCK_
  	exit 148 
 }
 
 _SITRPQuIT_() { # run on quit
 	local RV="$?"
-	printf "\\n%s build.github.what.is.it.bash\\n" "$RV" 
+	printf "\\e[?25h\\e[1;7;38;5;0mBuildAPKs build.github.what.is.it.bash %s WARNING:  Quit signal %s received!\\e[0m\\n" "${0##*/}" "$RV"
         printf "\\e[?25h\\n\\e[1;48;5;138mBuildAPKs build.github.what.is.it.bash %s WARNING:  Quit signal %s received near or at line number %s by \`%s\`!\\e[0m\\n" "${0##*/}" "${1:-UNDEFINED}" "${2:-LINENO}" "${3:-BASH_COMMAND}"
 	_WAKEUNLOCK_
  	exit 149 
@@ -37,17 +37,16 @@ _SITRPQuIT_() { # run on quit
 
 trap '_SITRPERROR_ $? $LINENO $BASH_COMMAND' ERR 
 trap _SITRPEXIT_ EXIT
-trap _SITRPSIGNAL_ HUP INT TERM 
+trap '_SITRPSIGNAL_ $? $LINENO $BASH_COMMAND'  HUP INT TERM
 trap '_SITRPQuIT_ $? $LINENO $BASH_COMMAND' QUIT 
 
 if [[ -z "${1:-}" ]] 
 then
-	printf "\\e[1;7;38;5;204m%s\\n\\e[0m\\n" "GitHub username must be provided;  See \`~/${RDR##*/}/conf/UNAMES\` for usernames that build APKs on device with BuildAPKs!  To build all the user names contained in this file run \`for i in \$(cat ~/${RDR##*/}/conf/UNAMES) ; do ~/${RDR##*/}/scripts/bash/build/build.github.users.bash \$i ; done\`.  File \`~/${RDR##*/}/conf/GAUTH\` has important information should you choose to run this command regarding bandwidth supplied by GitHub. "
+	printf "\\e[1;7;38;5;204m%s\\n\\e[0m\\n" "GitHub name must be provided;  See \`~/${RDR##*/}/conf/UNAMES\` for usernames that build APKs on device with BuildAPKs!  To build all the user names contained in this file run \`for i in \$(cat ~/${RDR##*/}/conf/UNAMES) ; do ~/${RDR##*/}/scripts/bash/build/${0##*/} \$i ; done\`.  File \`~/${RDR##*/}/conf/GAUTH\` has important information should you choose to run this command regarding bandwidth supplied by GitHub. "
 	exit 227
 fi
-export U="${1%/}"
-export UI="${U##*/}"
-export UIT="${UI,,}"
+export UI="${1%/}"
+export UIT="${UI##*/}"
 read TYPE < <(curl "https://api.github.com/users/$UIT/repos" -s 2>&1 | head -n 25 | tail -n 1 | grep -o Organization) # https://stackoverflow.com/questions/2559076/how-do-i-redirect-output-to-a-variable-in-shell/
 if [[ "$TYPE" == Organization ]]
 then
