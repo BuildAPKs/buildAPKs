511a7d709f150b80f81f53b560667a7c0bfa1497
SDRausty <27742457+SDRausty@users.noreply.github.com>
2019-10-01 09:37:02 -0400

    modified:   scripts/bash/init/pull.buildAPKs.modules.bash

--- scripts/bash/init/pull.buildAPKs.modules.bash
+++ scripts/bash/init/pull.buildAPKs.modules.bash
@@ -3,9 +3,42 @@
 # by buildAPKs https://buildapks.github.io/docsBuildAPKs/
 # Update repository and update submodules.
 #####################################################################
-set -e
+set -Eeuo pipefail
+shopt -s nullglob globstar
 
-declare -A SBMS # Declare associative array for available submoldules. 
+_SPTRPERROR_() { # run on script error
+	local RV="$?"
+	printf "\\e[?25h\\e[1;7;38;5;0mBuildAPKs pull.buildAPKs.modules.bash %s WARNING:  Error %s received!\\e[0m\\n" "${0##*/}" "$RV"
+	printf "\\e[?25h\\n\\e[1;48;5;138mBuildAPKs pull.buildAPKs.modules.bash %s %s ERROR:  Generated script error %s near or at line number %s by \`%s\`!\\e[0m\\n" "${0##*/}" "${PWD##*/}" "${1:-UNDEF}" "${2:-LINENO}" "${3:-BASH_COMMAND}"
+	exit 147
+}
+
+_SPTRPEXIT_() { # run on exit
+	printf "\\e[?25h\\e[0m"
+	set +Eeuo pipefail 
+	exit 0
+}
+
+_SPTRPSIGNAL_() { # run on signal
+	local RV="$?"
+	printf "\\e[?25h\\e[1;7;38;5;0mBuildAPKs pull.buildAPKs.modules.bash %s WARNING:  Signal %s received!\\e[0m\\n" "${0##*/}" "$RV"
+        printf "\\e[?25h\\n\\e[1;48;5;138mBuildAPKs pull.buildAPKs.modules.bash %s WARNING:  Signal %s received near or at line number %s by \`%s\`!\\e[0m\\n" "${0##*/}" "${1:-UNDEFINED}" "${2:-LINENO}" "${3:-BASH_COMMAND}"
+ 	exit 148 
+}
+
+_SPTRPQuIT_() { # run on quit
+	local RV="$?"
+	printf "\\e[?25h\\e[1;7;38;5;0mBuildAPKs pull.buildAPKs.modules.bash %s WARNING:  Quit signal %s received!\\e[0m\\n" "${0##*/}" "$RV"
+        printf "\\e[?25h\\n\\e[1;48;5;138mBuildAPKs pull.buildAPKs.modules.bash %s WARNING:  Quit signal %s received near or at line number %s by \`%s\`!\\e[0m\\n" "${0##*/}" "${1:-UNDEFINED}" "${2:-LINENO}" "${3:-BASH_COMMAND}"
+ 	exit 149 
+}
+
+trap '_SPTRPERROR_ $? $LINENO $BASH_COMMAND' ERR 
+trap _SPTRPEXIT_ EXIT
+trap '_SPTRPSIGNAL_ $? $LINENO $BASH_COMMAND'  HUP INT TERM
+trap '_SPTRPQuIT_ $? $LINENO $BASH_COMMAND' QUIT 
+
+declare -A SBMS # declare associative array for available submoldules
 RDR="$HOME/buildAPKs"
 SBMS=([scripts/bash/shlibs]="$RDR/scripts/bash/shlibs/.git" [sources/applications]="$RDR/sources/applications/.git" [sources/browsers]="$RDR/sources/browsers/.git" [sources/clocks]="$RDR/sources/clocks/.git" [sources/compasses]="$RDR/sources/compasses/.git" [sources/entertainment]="$RDR/sources/entertainment/.git" [sources/flashlights4]="$RDR/sources/flashlights4/.git" [sources/games]="$RDR/sources/games/.git" [sources/live.wallpapers]="$RDR/sources/live.wallpapers/.git" [sources/samples4]="$RDR/sources/samples4/.git" [sources/samps]="$RDR/sources/samps/.git" [sources/top10]="$RDR/sources/top10/.git" [sources/tools]="$RDR/sources/tools/.git" [sources/torches]="$RDR/sources/torches/.git" [sources/tutorials]="$RDR/sources/tutorials/.git" [sources/widgets]="$RDR/sources/widgets/.git")
 
