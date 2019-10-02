383427eac34cb2b2a9ba0f0897a1d9461c335409
SDRausty <27742457+sdrausty@users.noreply.github.com>
2019-10-02 08:20:05 -0400

    modified:   scripts/bash/build/build.buildAPKs.bash

--- scripts/bash/build/build.buildAPKs.bash
+++ scripts/bash/build/build.buildAPKs.bash
@@ -8,7 +8,6 @@ shopt -s nullglob globstar
 _SETRPERROR_() { # Run on script error.
 	local RV="$?"
 	printf "\\e[?25h\\e[1;7;38;5;0mbuildAPKs %s ERROR:  Signal %s received!\\e[0m\\n" "${0##*/}" "$RV"
-	echo exit 201
 	exit 201
 }
 
@@ -21,14 +20,12 @@ _SETRPEXIT_() { # Run on exi
 _SETRPSIGNAL_() { # Run on signal.
 	local RV="$?"
 	printf "\\e[?25h\\e[1;7;38;5;0mbuildAPKs %s WARNING:  Signal %s received!\\e[0m\\n" "${0##*/}" "$RV"
-	echo exit 211
  	exit 211 
 }
 
 _SETRPQUIT_() { # Run on quit.
 	local RV="$?"
 	printf "\\e[?25h\\e[1;7;38;5;0mbuildAPKs %s WARNING:  Quit signal %s received!\\e[0m\\n" "${0##*/}" "$RV"
-	echo exit 221
  	exit 221 
 }
 
@@ -38,8 +35,9 @@ trap _SETRPSIGNAL_ HUP INT TERM
 trap _SETRPQUIT_ QUIT 
 
 export RDR="$HOME/buildAPKs"
+export SDR="/scripts/bash/build"
 declare -a LIST # declare array for all build scripts 
-LIST=("$RDR/scripts/bash/build/build.apps.bash" "$RDR/scripts/bash/build/build.clocks.bash" "$RDR/scripts/bash/build/build.compasses.bash" "$RDR/scripts/bash/build/build.developers.tools.bash" "$RDR/scripts/bash/build/build.entertainment.bash" "$RDR/scripts/bash/build/build.flashlights.bash" "$RDR/scripts/bash/build/build.games.bash" "$RDR/scripts/bash/build/build.live.wallpapers.bash" "$RDR/scripts/bash/build/build.samples.bash" "$RDR/scripts/bash/build/buildApplications.bash" "$RDR/scripts/bash/build/buildBrowsers.bash" "$RDR/scripts/bash/build/buildFlashlights.bash" "$RDR/scripts/bash/build/buildGames.bash" "$RDR/scripts/bash/build/buildSamples.bash" "$RDR/scripts/bash/build/buildTop10.bash" "$RDR/scripts/bash/build/buildTutorials.bash" "$RDR/scripts/bash/build/buildWidgets.bash" "$RDR/scripts/bash/build/buildAll.bash")
+LIST=("$RDR$SDR/build.apps.bash" "$RDR$SDR/build.clocks.bash" "$RDR$SDR/build.compasses.bash" "$RDR$SDR/build.developers.tools.bash" "$RDR$SDR/build.entertainment.bash" "$RDR$SDR/build.flashlights.bash" "$RDR$SDR/build.games.bash" "$RDR$SDR/build.live.wallpapers.bash" "$RDR$SDR/build.samples.bash" "$RDR$SDR/buildApplications.bash" "$RDR$SDR/buildBrowsers.bash" "$RDR$SDR/buildFlashlights.bash" "$RDR$SDR/buildGames.bash" "$RDR$SDR/buildSamples.bash" "$RDR$SDR/buildTop10.bash" "$RDR$SDR/buildTutorials.bash" "$RDR$SDR/buildWidgets.bash" "$RDR$SDR/buildAll.bash")
 for NAME in "${LIST[@]}"
 do
 	"$NAME"
