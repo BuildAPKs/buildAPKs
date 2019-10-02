e15d8d2467bd53550ed3c41302195397dba2be24
SDRausty <27742457+sdrausty@users.noreply.github.com>
2019-10-02 08:37:03 -0400

    modified:   scripts/bash/init/pull.buildAPKs.modules.bash

--- scripts/bash/init/pull.buildAPKs.modules.bash
+++ scripts/bash/init/pull.buildAPKs.modules.bash
@@ -73,7 +73,7 @@ _CK4MS_() { # ChecKs 4 ModuleS
 
 _GSMU_() {	
 	printf "\\nUpdating buildAPKs; \`%s\` shall attempt to load sources from Github submodule repositories into ~/buildAPKs.  This may take a little while to complete.  Please be patient while \`%s\` downloads source code from https://github.com\\n" "${0##*/}" "${0##*/}"
-	(git pull && printf "\\nPlease wait; Updating ~/buildAPKs...\\n") || (printf "\\nCannot update ~/buildAPKs:  Continuing...\\n")
+	(git pull && printf "Please wait; Updating ~/buildAPKs...\\n") || (printf "Cannot update ~/buildAPKs:  Continuing...\\n")
 	for LOC in "${!SBMS[@]}" 
 	do
 		_GSU_ $LOC
@@ -81,7 +81,7 @@ _GSMU_() {
 }
 
 _GSU_() { # update submodules to latest version
-	(git submodule update --init --recursive --remote $1) ||  (printf "\\n\\e[1;7;38;5;66m%s\\e[0m\\n" "Cannot update ~/buildAPKs/$1:  Continuing...") # https://www.tecmint.com/chaining-operators-in-linux-with-practical-examples/
+	(git submodule update --init --recursive --remote $1) ||  (printf "\\e[1;7;38;5;66m%s\\e[0m\\n" "Cannot update ~/buildAPKs/$1:  Continuing...") # https://www.tecmint.com/chaining-operators-in-linux-with-practical-examples/
 }
 
 cd "$RDR/"
