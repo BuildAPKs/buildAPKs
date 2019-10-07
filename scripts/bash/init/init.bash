d19b0cd61d237ad579a308c7b9161fa3fa0acbed
SDRausty <27742457+SDRausty@users.noreply.github.com>
2019-10-06 20:23:35 -0400

    modified:   md5sum.sum 	modified:   scripts/bash/init/init.bash 	modified:   sha1sum.sum 	modified:   sha256sum.sum 	modified:   sha512sum.sum

--- scripts/bash/init/init.bash
+++ scripts/bash/init/init.bash
@@ -50,11 +50,14 @@ export NUM="$(date +%s)"
 export SRDR="${RDR##*/}" # search: string manipulation site:www.tldp.org
 export JDR="$RDR/sources/$JID"
 cd "$RDR"
+if [[ ! -f scripts/bash/shlibs/.git ]] 
+then
+	(git pull) || (printf "\\nCANNOT UPDATE ~/%s: Continuing...\\n\\n" "${RDR##*/}")
+fi
 if [[ ! -f .gitmodules ]] 
 then
 	touch .gitmodules
 fi
-(git pull) || (printf "\\nCANNOT UPDATE ~/%s: Continuing...\\n\\n" "${RDR##*/}") 
 if grep shlibs .gitmodules 1>/dev/null
 then
 	(git submodule update --init --recursive --remote scripts/bash/shlibs) || (printf "\\nCANNOT UPDATE ~/%s/scripts/bash/shlibs: Continuing...\\n\\n" "${RDR##*/}") 
@@ -65,5 +68,6 @@ if [[ ! -d "scripts/bash/shlibs" ]]
 then
 	(git clone https://github.com/shlibs/shlibs.bash scripts/bash/shlibs && git clone https://github.com/shlibs/shlibs.buildAPKs.bash scripts/bash/shlibs/buildAPKs) || (printf "\\nCANNOT CLONE MODULES %s AND %s INTO~/%s/scripts/bash/shlibs AND ~/%s/scripts/bash/shlibs/buildAPKs: Continuing...\\n\\n" "https://github.com/shlibs/shlibs.bash" "https://github.com/shlibs/shlibs.buildAPKs.bash" "${RDR##*/}" "${RDR##*/}")
 fi
+. "$RDR/scripts/bash/init/prep.bash"
 . "$RDR/scripts/bash/shlibs/mod.bash"
 # init.bash EOF
