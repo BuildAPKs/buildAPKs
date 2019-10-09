2fc7894cbfb55cd437964a43cb1d1766905bd342
SDRausty <27742457+SDRausty@users.noreply.github.com>
2019-10-08 20:19:57 -0400

    modified:   do.sums.bash 	modified:   trees.md5sum.sum 	modified:   trees.sha1sum.sum 	modified:   trees.sha224sum.sum 	modified:   trees.sha256sum.sum 	modified:   trees.sha384sum.sum 	modified:   trees.sha512sum.sum

--- do.sums.bash
+++ do.sums.bash
@@ -1,24 +1,24 @@
 #!/bin/env bash
 # Copyright 2019 (c) all rights reserved
 # by S D Rausty https://sdrausty.github.io
-# Invocation0 creates trees.*.sum files: ./do.sums.bash
-# Invocation1 checks md5sum: md5sum.sum -c trees.md5sum.sum
+# Checks sha512sum files: sha512sum -c trees.sha512sum.sum
+# Creates trees.*.sum files: ./do.sums.bash
 #####################################################################
 set -eu
 rm -f *.sum
 FILELIST=( $(find . -type f | grep -v .git | sort) )
 CHECKLIST=("md5sum" "sha1sum" "sha224sum" "sha256sum" "sha384sum" "sha512sum")
-for FILE in "${FILELIST[@]}"
+for SCHECK in "${CHECKLIST[@]}"
 do
-	for SCHECK in "${CHECKLIST[@]}"
+	for FILE in "${FILELIST[@]}"
 	do
  		printf "%s\\n" "Creating $SCHECK for $FILE..."
-		$SCHECK $FILE >> trees.$SCHECK.sum
+		"$SCHECK" "$FILE" >> trees."$SCHECK".sum
 	done
 done
 for SRCHECK in  "${CHECKLIST[@]}"
 do
 	printf "\\nChecking $SRCHECK...\\n"
-	$SRCHECK -c trees.$SRCHECK.sum
+	"$SRCHECK" -c trees."$SRCHECK".sum
 done
 # do.sums.sh EOF
