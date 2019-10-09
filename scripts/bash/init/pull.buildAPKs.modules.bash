7f073340de72dd83db7d0c1c8df0a93b136323f4
SDRausty <27742457+sdrausty@users.noreply.github.com>
2019-10-09 00:26:04 -0400

    modified:   scripts/bash/init/pull.buildAPKs.modules.bash 	modified:   trees.md5sum.sum 	modified:   trees.sha1sum.sum 	modified:   trees.sha224sum.sum 	modified:   trees.sha256sum.sum 	modified:   trees.sha384sum.sum 	modified:   trees.sha512sum.sum

--- scripts/bash/init/pull.buildAPKs.modules.bash
+++ scripts/bash/init/pull.buildAPKs.modules.bash
@@ -78,7 +78,7 @@ _GSMU_() {
 		if [[ -f "$RDR/$LOC/ma.bash" ]]
 		then
 			export JDR="$RDR/$LOC/"
-			bash "$RDR/$LOC/ma.bash"
+			. "$RDR/$LOC/ma.bash"
 		fi
 		if [[ -f "$RDR/$LOC/.gitmodules" ]] || [[ -f "$RDR/$LOC/ma.bash" ]]
 		then
@@ -105,7 +105,6 @@ then
 	(printf "\\e[1;7;38;5;96m%s\\e[0m\\n" "Updating ~/${RDR##*/}, ~/${RDR##*/}/scripts/bash/shlibs and ~/${RDR##*/}/scripts/bash/shlibs/buildAPKs..." && git pull && _GSUSHLIBS_ ) ||  (printf "\\e[1;7;38;5;66m%s\\e[0m\\n" "Cannot update ~/${RDR##*/}, ~/${RDR##*/}/scripts/bash/shlibs and ~/${RDR##*/}/scripts/bash/shlibs/buildAPKs: Continuing...")
 fi
 . "$RDR"/scripts/bash/shlibs/buildAPKs/prep.bash
-. "$RDR"/scripts/bash/shlibs/lock.bash
 if [[ ! -d "$RDR/sources" ]]
 then
 	mkdir -p "$RDR/sources"
