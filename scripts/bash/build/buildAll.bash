862469ae5df46636478bdec9dc48d6eab40b8da2
SDRausty <27742457+SDRausty@users.noreply.github.com>
2019-10-11 10:18:32 -0400

    modified:   scripts/bash/build/buildAll.bash 	modified:   ztree.sha512.sum

--- scripts/bash/build/buildAll.bash
+++ scripts/bash/build/buildAll.bash
@@ -38,7 +38,7 @@ trap _SETRPSIGNAL_ HUP INT TERM
 trap _SETRPQUIT_ QUIT 
 
 RDR="$HOME/buildAPKs"
-"$RDR"/scripts/bash/init/ushlibs.bash
+. "$RDR"/scripts/bash/init/ushlibs.bash
 "$RDR"/scripts/bash/shlibs/buildAPKs/pull.buildAPKs.modules.bash
 cd "$RDR"/sources/
 "$RDR"/scripts/bash/build/build.in.dir.bash
