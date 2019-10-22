df74cdf434954b7bb4818c7899aa8a2e7bb98cca
SDRausty <27742457+SDRausty@users.noreply.github.com>
2019-10-22 06:46:51 -0400

    deleted:    build.apps.bash 	deleted:    build.buildAPKs.bash 	deleted:    build.clocks.bash 	deleted:    build.compasses.bash 	deleted:    build.developers.tools.bash 	deleted:    build.entertainment.bash 	deleted:    build.flashlights.bash 	deleted:    build.games.bash 	deleted:    build.github.bash 	deleted:    build.live.wallpapers.bash 	deleted:    build.samples.bash 	deleted:    buildAll.bash 	renamed:    do.sum.bash -> do.sums.bash 	new file:   z.sha512.sum

--- do.sums.bash
+++ do.sums.bash
@@ -1,32 +1,32 @@
 #!/bin/env bash
 # Copyright 2019 (c) all rights reserved by S D Rausty; see LICENSE  
 # https://sdrausty.github.io hosted courtesy https://pages.github.com
-# To create the checksum file and commit use; ./do.sum.bash
-# To see the file tree use; awk '{ print $2 }' md5.sum
-# To check the files use; md5sum -c md5.sum
+# To create checksum files and commit use; ./do.sums.bash
+# To see file tree use; awk '{print $2}' z.sha512.sum
+# To check the files use; sha512sum -c z.sha512.sum
 #####################################################################
 set -eu
 git pull
-rm -f *.sum
-FILELIST=( $(find . -type f | grep -vw .git | sort) )
+rm -f z.*.sum
+FILELIST=( $(find . -type f | grep -v .git | sort) )
 CHECKLIST=(sha512sum)
 for SCHECK in ${CHECKLIST[@]}
 do
  	printf "%s\\n" "Creating $SCHECK file: Please wait a moment..."
 	for FILE in "${FILELIST[@]}"
 	do
-		$SCHECK "$FILE" >> .${SCHECK::-3}.sum
+		$SCHECK "$FILE" >> z.${SCHECK::-3}.sum
 	done
-	chmod 400 .${SCHECK::-3}.sum
 done
+chmod 400 z.${SCHECK::-3}.sum 
 for SCHECK in  ${CHECKLIST[@]}
 do
 	printf  "\\n%s\\n" "Checking $SCHECK..."
-	$SCHECK -c .${SCHECK::-3}.sum
+	$SCHECK -c z.${SCHECK::-3}.sum
 done
 git add .
 git commit
 git push
 ls
 printf "\\e[1;38;5;112m%s\\e[0m\\n" "$PWD"
-# do.sum.sh EOF
+# do.sums.sh EOF
