67b247c661f385470cb196f32240a180f8853927
SDRausty <27742457+sdrausty@users.noreply.github.com>
2019-10-23 10:35:32 -0400

    modified:   do.sums.bash 	renamed:    z.sha512.sum -> sha512.sum

--- do.sums.bash
+++ do.sums.bash
@@ -2,12 +2,12 @@
 # Copyright 2019 (c) all rights reserved by S D Rausty; see LICENSE  
 # https://sdrausty.github.io hosted courtesy https://pages.github.com
 # To create checksum files and commit use; ./do.sums.bash
-# To see file tree use; awk '{print $2}' z.sha512.sum
-# To check the files use; sha512sum -c z.sha512.sum
+# To see file tree use; awk '{print $2}' sha512.sum
+# To check the files use; sha512sum -c sha512.sum
 #####################################################################
 set -eu
 git pull
-rm -f z.*.sum
+rm -f *.sum
 FILELIST=( $(find . -type f | grep -v .git | sort) )
 CHECKLIST=(sha512sum)
 for SCHECK in ${CHECKLIST[@]}
@@ -15,14 +15,14 @@ do
  	printf "%s\\n" "Creating $SCHECK file: Please wait a moment..."
 	for FILE in "${FILELIST[@]}"
 	do
-		$SCHECK "$FILE" >> z.${SCHECK::-3}.sum
+		$SCHECK "$FILE" >> ${SCHECK::-3}.sum
 	done
 done
-chmod 400 z.${SCHECK::-3}.sum 
+chmod 400 ${SCHECK::-3}.sum 
 for SCHECK in  ${CHECKLIST[@]}
 do
 	printf  "\\n%s\\n" "Checking $SCHECK..."
-	$SCHECK -c z.${SCHECK::-3}.sum
+	$SCHECK -c ${SCHECK::-3}.sum
 done
 git add .
 git commit
