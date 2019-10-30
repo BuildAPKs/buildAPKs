d3b5e2246fd600d9276732d8fd4219283636e364
SDRausty <27742457+SDRausty@users.noreply.github.com>
2019-10-30 00:47:37 -0400

    new file:   .conf/VERSIONID 	modified:   scripts/maintenance/vgen.sh 	modified:   sha512.sum 	deleted:    var/conf/VERSIONID 	renamed:    var/conf/CNAMES -> var/db/CNAMES 	renamed:    var/conf/ENAMES -> var/db/ENAMES 	renamed:    var/conf/GAUTH -> var/db/GAUTH 	renamed:    var/conf/NUNAMES -> var/db/NUNAMES 	renamed:    var/conf/ONAMES -> var/db/ONAMES 	renamed:    var/conf/PNAMES -> var/db/PNAMES 	renamed:    var/conf/README.md -> var/db/README.md 	renamed:    var/conf/TNAMES -> var/db/TNAMES 	renamed:    var/conf/UNAMES -> var/db/UNAMES 	renamed:    var/conf/ZNAMES -> var/db/ZNAMES 	renamed:    var/conf/rm.dups.sh -> var/db/rm.dups.sh

--- scripts/maintenance/vgen.sh
+++ scripts/maintenance/vgen.sh
@@ -3,10 +3,10 @@
 # https://sdrausty.github.io hosted courtesy https://pages.github.com
 #####################################################################
 set -eu
-IVIDV=$(cat ./var/conf/VERSIONID)
+IVIDV=$(cat .conf/VERSIONID)
 PVIDV=${IVIDV%.*} # http://www.gnu.org/savannah-checkouts/gnu/bash/manual/bash.html#Shell-Parameter-Expansion 
 SVIDV=${IVIDV##*.} 
 NSVIDV=$((SVIDV + 1))
-echo $PVIDV.$NSVIDV > ./var/conf/VERSIONID 
-cat ./var/conf/VERSIONID 
+echo $PVIDV.$NSVIDV > .conf/VERSIONID 
+cat .conf/VERSIONID 
 # vgen.sh EOF
