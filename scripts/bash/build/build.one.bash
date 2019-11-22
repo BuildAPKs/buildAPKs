8618a82ea345361288461bfad99f2df2c430b18f
SDRausty <27742457+SDRausty@users.noreply.github.com>
2019-11-22 07:18:09 -0500

    commit 542733357 in 2019

--- scripts/bash/build/build.one.bash
+++ scripts/bash/build/build.one.bash
@@ -22,8 +22,8 @@ _SBOTRPEXIT_() { # run on exit
 	if [[ "$RV" != 0 ]]  
 	then 
 		printf "\\e[?25h\\e[1;7;38;5;0mbuildAPKs signal %s received by %s in %s by build.one.bash.  More information in \`%s/var/log/stnderr.%s.log\` file.\\n\\n" "$RV" "${0##*/}" "$PWD" "$RDR" "$JID" 
-		printf "%s\\n" "Running: grep -C 2 ERROR $RDR/var/log/stnderr.$JID.log | tail "
-		grep -C 2 ERROR "$RDR/var/log/stnderr.$JID.log" | tail 
+		printf "%s\\n" "Running: grep -iC 4 ERROR $RDR/var/log/stnderr.$JID.log | tail "
+		grep -iC 4 ERROR "$RDR/var/log/stnderr.$JID.log" | tail 
 		printf "\\e[0m\\n\\n" 
 	fi
 	if [[ "$RV" = 220 ]]  
