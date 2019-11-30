301579a9900eb1635073e6cd9521be855b2973c6
SDRausty <27742457+sdrausty@users.noreply.github.com>
2019-11-30 05:56:20 -0500

    change 170248853 on 20191130

--- scripts/bash/init/setup.buildAPKs.bash
+++ scripts/bash/init/setup.buildAPKs.bash
@@ -65,7 +65,7 @@ STRING2="Cannot update ~/${RDR##*/} prerequisite: Continuing..."
 printf "\\e[1;38;5;115m%s\\e[0m\\n" "Beginning buildAPKs setup:"
 COMMANDR="$(command -v au)" || (printf "%s\\n\\n" "$STRING1") 
 COMMANDIF="${COMMANDR##*/}"
-PKGS=(aapt apksigner curl dx ecj git)
+PKGS=(aapt apksigner curl dx ecj findutils git)
 if [[ -z "${1:-}" ]]
 then
 	ARGS=""
