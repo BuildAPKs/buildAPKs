ebf388d9c40f90984712cb0b356d491201c825f3
SDRausty <27742457+sdrausty@users.noreply.github.com>
2019-12-01 22:16:10 -0500

    commit 366372551 in 2019

--- scripts/maintenance/do.sums.bash
+++ scripts/maintenance/do.sums.bash
@@ -1,21 +1,21 @@
 #!/bin/env bash
 # Copyright 2019 (c) all rights reserved by S D Rausty; see LICENSE  
 # https://sdrausty.github.io hosted courtesy https://pages.github.com
-# You can use ` do.sums.bash ` to commit and create checksum files.
+# To create checksum files and commit use; ./do.sums.bash
 # To see file tree use; awk '{print $2}' sha512.sum
 # To check the files use; sha512sum -c sha512.sum
 #####################################################################
 set -eu
-MTIME="$(ls -l --time-style=+"%s" .git/FETCH_HEAD | awk '{print $6}')"
+MTIME="$(ls -l --time-style=+"%s" .git/ORIG_HEAD 2>/dev/null | awk '{print $6}')"
 TIME="$(date +%s)"
-(if [[ $(($TIME - $MTIME)) -gt 43200 ]] ; then git pull ; fi) || git pull
+([[ ! -z "${MTIME##*[!0-9]*}" ]] && (if [[ $(($TIME - $MTIME)) -gt 43200 ]] ; then git pull ; fi) || git pull) || (printf "%s\\n" "Signal generated at [ ! -z \${num##*[!0-9]*} ]" && git pull)
 ./scripts/maintenance/vgen.sh
 rm -f *.sum
-FILELIST=( $(find . -type f | grep -wv .git | sort) )
+FILELIST=( $(find . -type f | grep -vw .git | sort) )
 CHECKLIST=(sha512sum) # md5sum sha1sum sha224sum sha256sum sha384sum
 for SCHECK in ${CHECKLIST[@]}
 do
- 	printf "%s\\n" "Creating $SCHECK file..."
+	printf "%s\\n" "Creating $SCHECK file..."
 	for FILE in "${FILELIST[@]}"
 	do
 		$SCHECK "$FILE" >> ${SCHECK::-3}.sum
@@ -24,11 +24,11 @@ done
 chmod 400 ${SCHECK::-3}.sum 
 for SCHECK in  ${CHECKLIST[@]}
 do
-	printf  "\\n%s\\n" "Checking $SCHECK..."
+	printf "%s\\n" "Checking $SCHECK..."
 	$SCHECK -c ${SCHECK::-3}.sum
 done
 git add .
-SN="$(sn.sh)" # sn.sh is found in maintenance.BuildAPKs
+SN="$(sn.sh)" # sn.sh is found in https://github.com/BuildAPKs/maintenance.BuildAPKs/blob/master/sn.sh
 git commit -m "$SN"
 git push
 ls
