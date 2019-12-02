aaa1fcf5f7dfb8c602b36cd9f6dd023c6d3d5e72
SDRausty <27742457+sdrausty@users.noreply.github.com>
2019-12-02 02:19:51 -0500

    commit 232802571 in 2019

--- scripts/bash/init/init.bash
+++ scripts/bash/init/init.bash
@@ -4,38 +4,8 @@
 #####################################################################
 set -Eeuo pipefail
 shopt -s nullglob globstar
-
-_SINITRPERROR_() { # run on script error
-	local RV="$?"
-	echo "$RV" init.bash
-	printf "\\e[?25h\\n\\e[1;48;5;138mBuildAPKs %s ERROR:  Generated script error %s near or at line number %s by \`%s\`!\\e[0m\\n" "${PWD##*/}" "${1:-UNDEF}" "${2:-LINENO}" "${3:-BASH_COMMAND}"
-	exit 147
-}
-
-_SINITRPEXIT_() { # run on exit
-	printf "\\e[?25h\\e[0m"
-	set +Eeuo pipefail 
-	exit
-}
-
-_SINITRPSIGNAL_() { # run on signal
-	local RV="$?"
-	printf "\\e[?25h\\e[1;7;38;5;0mBuildAPKs %s WARNING:  Signal %s received!\\e[0m\\n" "init.bash" "$RV"
- 	exit 148 
-}
-
-_SINITRPQUIT_() { # run on quit
-	local RV="$?"
-	printf "\\e[?25h\\e[1;7;38;5;0mBuildAPKs %s WARNING:  Quit signal %s received!\\e[0m\\n" "init.bash" "$RV"
- 	exit 149 
-}
-
-trap '_SINITRPERROR_ $? $LINENO $BASH_COMMAND' ERR 
-trap _SINITRPEXIT_ EXIT
-trap _SINITRPSIGNAL_ HUP INT TERM 
-trap _SINITRPQUIT_ QUIT 
-
 export RDR="$HOME/buildAPKs"   
+. "$RDR"/scripts/bash/init/atrap.bash 147 148 149 "${0##*/} init.bash" 
 cd "$RDR"
 if [[ -z "${JID:-}" ]] 
 then
