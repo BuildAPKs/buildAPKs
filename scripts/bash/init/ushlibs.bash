b6da53b42e59842eb85f00ad66157615409e8a99
SDRausty <27742457+sdrausty@users.noreply.github.com>
2019-12-02 02:55:59 -0500

    commit 118582673 in 2019

--- scripts/bash/init/ushlibs.bash
+++ scripts/bash/init/ushlibs.bash
@@ -5,37 +5,7 @@
 #####################################################################
 set -Eeuo pipefail
 shopt -s nullglob globstar
-
-_SRSTRPERROR_() { # run on script error
-	local RV="$?"
-	echo "$RV" init.bash
-	printf "\\e[?25h\\e[1;48;5;138mBuildAPKs %s ERROR:  Generated script error %s near or at line number %s by \`%s\`!\\e[0m\\n" "${PWD##*/}" "${1:-UNDEF}" "${2:-LINENO}" "${3:-BASH_COMMAND}"
-	exit 147
-}
-
-_SRSTRPEXIT_() { # run on exit
-	printf "\\e[?25h\\e[0m"
-	set +Eeuo pipefail 
-	exit
-}
-
-_SRSTRPSIGNAL_() { # run on signal
-	local RV="$?"
-	printf "\\e[?25h\\e[1;7;38;5;0mBuildAPKs %s WARNING:  Signal %s received!\\e[0m\\n" "init.bash" "$RV"
- 	exit 148 
-}
-
-_SRSTRPQUIT_() { # run on quit
-	local RV="$?"
-	printf "\\e[?25h\\e[1;7;38;5;0mBuildAPKs %s WARNING:  Quit signal %s received!\\e[0m\\n" "init.bash" "$RV"
- 	exit 149 
-}
-
-trap '_SRSTRPERROR_ $? $LINENO $BASH_COMMAND' ERR 
-trap _SRSTRPEXIT_ EXIT
-trap _SRSTRPSIGNAL_ HUP INT TERM 
-trap _SRSTRPQUIT_ QUIT 
-
+. "$RDR"/scripts/bash/init/atrap.bash 137 138 139 "${0##*/} ushlibs.bash" 
 _IFSHLIBS_() { 
 	if [[ ! -d "$RDR"/scripts/bash/shlibs ]] 
 	then
