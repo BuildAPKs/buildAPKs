8970ebffdccc20c2cbd34568d7eed4116e9740bb
SDRausty <27742457+sdrausty@users.noreply.github.com>
2019-12-04 09:28:12 -0500

    change 759915801 on 20191204

--- scripts/bash/build/build.one.bash
+++ scripts/bash/build/build.one.bash
@@ -4,7 +4,7 @@
 #####################################################################
 set -Eeuo pipefail
 shopt -s nullglob globstar
-. "$RDR"/scripts/bash/shlibs/buildAPKs/fapks.bash
+
 _SBOTRPERROR_() { # run on script error
 	local RV="$?"
 	echo $RV build.one.bash  
@@ -154,5 +154,34 @@ aapt add -f "$PKGNAM.apk" classes.dex
 printf "\\e[1;38;5;114m%s\\e[1;38;5;108m\\n" "Signing $PKGNAM.apk..."
 apksigner ../"$PKGNAM-debug.key" "$PKGNAM.apk" ../"$PKGNAM.apk"
 cd ..
-_CPAPK_ # copy APK file 
+if [[ -w "/storage/emulated/0/" ]] ||  [[ -w "/storage/emulated/legacy/" ]]  
+then
+	if [[ -w "/storage/emulated/0/" ]] 
+	then
+		if [[ ! -d "/storage/emulated/0/Download/builtAPKs/$JID.$DAY" ]]
+		then
+			mkdir -p "/storage/emulated/0/Download/builtAPKs/$JID.$DAY"
+		fi
+		cp "$PKGNAM.apk" "/storage/emulated/0/Download/builtAPKs/$JID.$DAY/$PKGNAME.apk"
+	fi
+	if [[ -w "/storage/emulated/legacy/" ]]  
+	then
+		if [[ ! -d "/storage/emulated/legacy/Download/builtAPKs/$JID.$DAY" ]]
+		then
+			mkdir -p "/storage/emulated/legacy/Download/builtAPKs/$JID.$DAY"
+		fi
+		cp "$PKGNAM.apk" "/storage/emulated/legacy/Download/builtAPKs/$JID.$DAY/$PKGNAME.apk"
+	fi
+	printf "\\e[1;38;5;115mCopied %s to Download/builtAPKs/%s/%s.apk\\n" "$PKGNAM.apk" "$JID.$DAY" "$PKGNAME"
+	printf "\\e[1;38;5;149mThe APK %s file can be installed from Download/builtAPKs/%s/%s.apk\\n" "$PKGNAM.apk" "$JID.$DAY" "$PKGNAME"
+else
+	if [[ ! -d "$RDR/var/cache/builtAPKs/$JID.$DAY" ]]
+	then
+		mkdir -p "$RDR/var/cache/builtAPKs/$JID.$DAY"
+	fi
+	cp "$PKGNAM.apk" "$RDR/var/cache/builtAPKs/$JID.$DAY/$PKGNAME.apk"
+	printf "\\e[1;38;5;120mCopied %s to $RDR/var/cache/builtAPKs/%s/%s.apk\\n" "$PKGNAM.apk" "$JID.$DAY" "$PKGNAME"
+	printf "\\e[1;38;5;154mThe APK %s file can be installed from ~/${RDR:33}/var/cache/builtAPKs/%s/%s.apk\\n" "$PKGNAM.apk" "$JID.$DAY" "$PKGNAME"
+fi
+printf "\\e[?25h\\e[1;7;38;5;34mShare %s everwhere%s!\\e[0m\\n" "https://wiki.termux.com/wiki/Development" "🌎🌍🌏🌐"
 # build.one.bash EOF
