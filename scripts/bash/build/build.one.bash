24c8d93150af44052b817f44eb0641073a113c34
SDRausty <27742457+SDRausty@users.noreply.github.com>
2019-12-08 03:59:30 -0500

    commit 041066341 in 2019

--- scripts/bash/build/build.one.bash
+++ scripts/bash/build/build.one.bash
@@ -141,8 +141,7 @@ sed -i "s/targetSdkVersion\=\"[0-9]\"/targetSdkVersion\=\"$TSDKVERSION\"/g" Andr
 sed -i "s/targetSdkVersion\=\"[0-9][0-9]\"/targetSdkVersion\=\"$TSDKVERSION\"/g" AndroidManifest.xml 
 printf "\\e[1;38;5;115m%s\\n\\e[0m" "aapt: started..."
 aapt package -f \
-	--min-sdk-version "$MSDKVERSION" \
-	--target-sdk-version "$TSDKVERSION" --version-code "$NOW" --version-name "$PKGNAM" -c "$(getprop persist.sys.locale|awk -F- '{print $1}')" \
+	--min-sdk-version "$MSDKVERSION" --target-sdk-version "$TSDKVERSION" --version-code "$NOW" --version-name "$PKGNAM" -c "$(getprop persist.sys.locale|awk -F- '{print $1}')" \
 	-M AndroidManifest.xml \
 	-J gen \
 	-S res
