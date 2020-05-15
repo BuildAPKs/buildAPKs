b4809de332991bfbddfce07079af384a22754907
SDRausty <27742457+SDRausty@users.noreply.github.com>
2020-05-15 08:44:54 -0400

    commit 051940630 on 20200515

--- scripts/bash/build/build.buildAPKs.bash
+++ scripts/bash/build/build.buildAPKs.bash
@@ -9,11 +9,13 @@ export RDR="$HOME/buildAPKs"
 export NUM="$(date +%s)"
 export RDR="$HOME/buildAPKs"
 export SDR="/scripts/bash/build"
-export OAUT="$(cat "$RDR/.conf/GAUTH" | awk 'NR==1')" # load login:token key from .conf/GAUTH file; This file has information about enabling OAUTH authentication.
+export OAUT="$(cat "$RDR/.conf/GAUTH" | awk 'NR==1')" # load login:token key from file .conf/GAUTH which has information about enabling OAUTH authentication.
 declare -a LIST # declare array for all build scripts 
-LIST=("$RDR$SDR/build.apps.bash" "$RDR$SDR/build.clocks.bash" "$RDR$SDR/build.compasses.bash" "$RDR$SDR/build.developers.tools.bash" "$RDR$SDR/build.entertainment.bash" "$RDR$SDR/build.flashlights.bash" "$RDR$SDR/build.games.bash" "$RDR$SDR/build.live.wallpapers.bash" "$RDR$SDR/build.samples.bash" "$RDR$SDR/buildApplications.bash" "$RDR$SDR/buildBrowsers.bash" "$RDR$SDR/buildFlashlights.bash" "$RDR$SDR/buildGames.bash" "$RDR$SDR/buildSamples.bash" "$RDR$SDR/buildTop10.bash" "$RDR$SDR/buildTutorials.bash" "$RDR$SDR/buildWidgets.bash") 
+LIST=("$(find ~/buildAPKs/scripts/bash/build/ -type f -name "*.bash")")
+cd "$RDR"
 . "$RDR/scripts/bash/shlibs/lock.bash" wake.start 
 . "$RDR/scripts/bash/shlibs/buildAPKs/bnchn.bash" bch.st 
+. "$RDR/scripts/bash/shlibs/buildAPKs/init/build.buildAPKs.modules.bash"
 for NAME in "${LIST[@]}"
 do
 	"$NAME"
