641bdf0deebf65ee8c7226548d793cca56a8fec3
SDRausty <27742457+SDRausty@users.noreply.github.com>
2020-05-16 17:42:02 -0400

    commit 825667801 on 20200516

--- scripts/bash/build/build.buildAPKs.bash
+++ scripts/bash/build/build.buildAPKs.bash
@@ -4,24 +4,20 @@
 ################################################################################
 set -Eeuo pipefail
 shopt -s nullglob globstar
+declare -a LIST # declare array for build scripts 
 export RDR="$HOME/buildAPKs"
 . "$RDR/scripts/bash/shlibs/trap.bash" 67 68 69 "${0##*/} build.buildAPKs.bash"
 export NUM="$(date +%s)"
 export RDR="$HOME/buildAPKs"
 export SDR="/scripts/bash/build"
-export OAUT="$(cat "$RDR/.conf/GAUTH" | awk 'NR==1')" # load login:token key from file .conf/GAUTH which has information about enabling OAUTH authentication.
-declare -a LIST # declare array for all build scripts 
-LIST=($(find ~/buildAPKs/scripts/bash/build/ -type f -name "*.bash"))
-cd "$RDR"
+export OAUT="$(cat "$RDR/.conf/GAUTH" | awk 'NR==1')" # load login:token key from file.  File RDR/.conf/GAUTH has more information about enabling OAUTH authentication.
 . "$RDR/scripts/bash/shlibs/lock.bash" wake.start 
 . "$RDR/scripts/bash/shlibs/buildAPKs/bnchn.bash" bch.st 
 . "$RDR/scripts/bash/shlibs/buildAPKs/init/build.buildAPKs.modules.bash"
+LIST=($(find "$RDR/scripts/bash/build/" -type f -name "*.bash" -not -name "build.buildAPKs.bash" -not -name "build.in.dir.bash"))
 for NAME in "${LIST[@]}"
 do
-	if [[ "${NAME##*/}" != build.buildAPKs.bash ]]
-	then
-		"$NAME"
-	fi
+	echo "$NAME"
 done
 . "$RDR/scripts/bash/shlibs/lock.bash" wake.stop 
 . "$RDR/scripts/bash/shlibs/buildAPKs/bnchn.bash" bch.gt 
