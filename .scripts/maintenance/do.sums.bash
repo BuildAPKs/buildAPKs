0446aad13f6133a4eddafc76c4154dfd9fe92f87
SDRausty <27742457+SDRausty@users.noreply.github.com>
2020-05-18 20:31:53 -0400

    commit 877068445 on 20200518

--- .scripts/maintenance/do.sums.bash
+++ .scripts/maintenance/do.sums.bash
@@ -29,7 +29,7 @@ do
 done
 git add .
 SN="$(sn.sh)" # sn.sh is located at https://github.com/BuildAPKs
-[[ -z "${1:-}" ]] && git commit -m "$SN" || [[ "${1//-}" = [Ss]* ]] && git commit -a -S -m "$SN" && pkill gpg-agent
+[[ -z "${1:-}" ]] && git commit -m "$SN" || [[ "${1//-}" = [Ss]* ]] && git commit -a -S -m "$SN" && pkill gpg-agent || git commit -m "$SN"  
 git push
 ls --color=always
 printf "%s\\n" "$PWD"
