27b13f3f1a4e3ea98bc495c8bb9d44ca88dae9ac
SDRausty <27742457+SDRausty@users.noreply.github.com>
2020-07-18 14:59:26 -0400

    commit 911671708 on 20200718

--- scripts/bash/build/build.one.bash
+++ scripts/bash/build/build.one.bash
@@ -159,7 +159,7 @@ printf "\\e[1;38;5;113m%s\\e[1;38;5;107m\\n" "Adding classes.dex $(find lib -typ
 aapt add -v -f "$PKGNAM.apk" classes.dex $(find lib -type f -name "*.so") 
 printf "\\e[1;38;5;114m%s\\e[1;38;5;108m\\n" "Signing $PKGNAM.apk..."
 apksigner sign --cert "$RDR/opt/key/certificate.pem" --key "$RDR/opt/key/key.pk8" "$PKGNAM.apk" 
-apksigner verify --verbose "$PKGNAME.apk" 
+apksigner verify --verbose "$PKGNAM.apk" 
 cd ..
 _COPYAPK_ || printf "%s\\n" "Unable to copy APK file ${0##*/} build.one.bash; Continuing..." 
 printf "\\e[?25h\\e[1;7;38;5;34mShare %s everwhere%s!\\e[0m\\n" "https://wiki.termux.com/wiki/Development" "🌎🌍🌏🌐"
