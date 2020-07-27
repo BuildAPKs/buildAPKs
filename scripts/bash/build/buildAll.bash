4311e9ed376044ff885951c96169006f2d21793d
SDRausty <27742457+SDRausty@users.noreply.github.com>
2020-07-27 12:16:01 -0400

    commit 505293266 on 20200727

--- scripts/bash/build/buildAll.bash
+++ scripts/bash/build/buildAll.bash
@@ -4,39 +4,8 @@
 #####################################################################
 set -Eeuo pipefail
 shopt -s nullglob globstar
-
-_SETRPERROR_() { # run on script error
-	local RV="$?"
-	printf "\\e[?25h\\e[1;7;38;5;0mbuildAPKs %s ERROR:  Signal %s received!\\e[0m\\n" "${0##*/}" "$RV"
-	echo exit 201
-	exit 201
-}
-
-_SETRPEXIT_() { # run on exit
-	printf "\\e[?25h\\e[0m"
-	set +Eeuo pipefail 
-	exit
-}
-
-_SETRPSIGNAL_() { # run on signal
-	local RV="$?"
-	printf "\\e[?25h\\e[1;7;38;5;0mbuildAPKs %s WARNING:  Signal %s received!\\e[0m\\n" "${0##*/}" "$RV"
-	echo exit 211
- 	exit 211 
-}
-
-_SETRPQUIT_() { # run on quit
-	local RV="$?"
-	printf "\\e[?25h\\e[1;7;38;5;0mbuildAPKs %s WARNING:  Quit signal %s received!\\e[0m\\n" "${0##*/}" "$RV"
-	echo exit 221
- 	exit 221 
-}
-
-trap '_SETRPERROR_ $LINENO $BASH_COMMAND $?' ERR 
-trap _SETRPEXIT_ EXIT
-trap _SETRPSIGNAL_ HUP INT TERM 
-trap _SETRPQUIT_ QUIT 
-
+export RDR="$HOME/buildAPKs"
+. "$RDR/scripts/bash/shlibs/trap.bash" 67 68 69 "${0##*/} buildAll.bash"
 export NUM="$(date +%s)"
 export RDR="$HOME/buildAPKs"
 cd "$RDR"
--- scripts/bash/build/buildTutorials.bash
+++ scripts/bash/build/buildTutorials.bash
@@ -4,36 +4,8 @@
 #####################################################################
 set -Eeuo pipefail
 shopt -s nullglob globstar
-
-_SETRPERROR_() { # Run on script error.
-	local RV="$?"
-	printf "\\e[?25h\\e[1;7;38;5;0mbuildAPKs %s ERROR:  Signal %s received!\\e[0m\\n" "${0##*/}" "$RV"
-	exit 201
-}
-
-_SETRPEXIT_() { # Run on exit.
-	printf "\\e[?25h\\e[0m"
-	set +Eeuo pipefail 
-	exit
-}
-
-_SETRPSIGNAL_() { # Run on signal.
-	local RV="$?"
-	printf "\\e[?25h\\e[1;7;38;5;0mbuildAPKs %s WARNING:  Signal %s received!\\e[0m\\n" "${0##*/}" "$RV"
- 	exit 211 
-}
-
-_SETRPQUIT_() { # Run on quit.
-	local RV="$?"
-	printf "\\e[?25h\\e[1;7;38;5;0mbuildAPKs %s WARNING:  Quit signal %s received!\\e[0m\\n" "${0##*/}" "$RV"
- 	exit 221 
-}
-
-trap '_SETRPERROR_ $LINENO $BASH_COMMAND $?' ERR 
-trap _SETRPEXIT_ EXIT
-trap _SETRPSIGNAL_ HUP INT TERM 
-trap _SETRPQUIT_ QUIT 
-
+export RDR="$HOME/buildAPKs"
+. "$RDR/scripts/bash/shlibs/trap.bash" 67 68 69 "${0##*/} build.tutorials.bash"
 export JAD=github.com/sdrausty/buildAPKsTutorials
 export JID=tutorials # job id/name
 . "$HOME/buildAPKs/scripts/bash/init/init.bash" "$@"
