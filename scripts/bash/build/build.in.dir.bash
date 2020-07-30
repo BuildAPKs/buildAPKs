7ceb9f98e4a452e23921c2a73b75b05b018d58fd
SDRausty <27742457+SDRausty@users.noreply.github.com>
2020-07-30 12:54:37 -0400

    commit 577050536 on 20200730

--- scripts/bash/build/build.in.dir.bash
+++ scripts/bash/build/build.in.dir.bash
@@ -4,8 +4,8 @@
 #####################################################################
 set -Eeuo pipefail
 shopt -s nullglob globstar
-[ "$PWD" = "$PREFIX" ] || [ "$PWD" = "$HOME" ] || [ "$PWD" = "$RDR" ] && printf "\\e[?25h\\e[1;7;38;5;0mSignal 224 generated in %s;  Cannot run in folder %s; %s exiting...\\e[0m\\n" "$PWD" "$PWD" "${0##*/} build.in.dir.bash" && exit 224
 export RDR="$HOME/buildAPKs"
+[ "$PWD" = "$PREFIX" ] || [ "$PWD" = "$HOME" ] || [ "$PWD" = "$RDR" ] && printf "\\e[?25h\\e[1;7;38;5;0mSignal 224 generated in %s;  Cannot run in folder %s; %s exiting...\\e[0m\\n" "$PWD" "$PWD" "${0##*/} build.in.dir.bash" && exit 224
 . "$RDR"/scripts/bash/init/atrap.bash 201 211 221 "${0##*/} build.in.dir.bash" wake.start
 export JDR="$PWD"
 export DAY="$(date +%Y%m%d)"
