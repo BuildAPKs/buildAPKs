7b095424f6705b24700f53bd2e1292cfefe32eb2
SDRausty <27742457+SDRausty@users.noreply.github.com>
2020-07-31 23:04:28 -0400

    commit 896689169 on 20200731

--- scripts/bash/build/buildAll.bash
+++ scripts/bash/build/buildAll.bash
@@ -8,10 +8,10 @@ export RDR="$HOME/buildAPKs"
 . "$RDR/scripts/bash/init/atrap.bash" 67 68 69 "${0##*/} buildAll.bash"
 export NUM="$(date +%s)"
 export RDR="$HOME/buildAPKs"
-cd "$RDR"
 . "$RDR"/scripts/bash/shlibs/lock.bash wake.start
 . "$RDR"/scripts/bash/init/ushlibs.bash
 . "$RDR"/scripts/bash/shlibs/buildAPKs/init/build.buildAPKs.modules.bash
+cd "$RDR"/sources/
 . "$RDR"/scripts/bash/build/build.in.dir.bash
 . "$RDR"/scripts/bash/shlibs/lock.bash wake.stop
 # buildAll.bash EOF
