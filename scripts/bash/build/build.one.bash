c5eebad5cd0a8b5c845db3bd0b0514f644013970
SDRausty <27742457+SDRausty@users.noreply.github.com>
2021-08-28 09:56:31 -0400

    commit 301310084 on 20210828

--- scripts/bash/build/build.one.bash
+++ scripts/bash/build/build.one.bash
@@ -127,7 +127,7 @@ PKGNAM="$(grep -o "package=.*" AndroidManifest.xml | cut -d\" -f2)"
 [ -f ./bin/"$PKGNAM.apk"  ] && rm ./bin/"$PKGNAM.apk"
 PKGNAME="$PKGNAM.$NOW"
 MSDKVERSION="$(getprop ro.build.version.min_supported_target_sdk)"
-PSYSLOCAL="$(getprop persist.sys.locale|awk -F- '{print $1}')"
+PSYSLOCALE="$(getprop persist.sys.locale|awk -F- '{print $1}')"
 TSDKVERSION="$(getprop ro.build.version.sdk)"
 sed -i "s/minSdkVersion\=\"[0-9]\"/minSdkVersion\=\"$MSDKVERSION\"/g" AndroidManifest.xml
 sed -i "s/minSdkVersion\=\"[0-9][0-9]\"/minSdkVersion\=\"$MSDKVERSION\"/g" AndroidManifest.xml
@@ -135,9 +135,7 @@ sed -i "s/targetSdkVersion\=\"[0-9]\"/targetSdkVersion\=\"$TSDKVERSION\"/g" Andr
 sed -i "s/targetSdkVersion\=\"[0-9][0-9]\"/targetSdkVersion\=\"$TSDKVERSION\"/g" AndroidManifest.xml
 printf "\\e[1;38;5;115m%s\\n\\e[0m" "aapt: started..."
 # begin build
-printf "%s\\n" "Generating R.java files..."
-aapt package -f --generate-dependencies --min-sdk-version "$MSDKVERSION" --target-sdk-version "$TSDKVERSION" --version-code "$NOW" --version-name "$PKGNAME" -c "$PSYSLOCAL" --non-constant-id -m -M AndroidManifest.xml -S ./res -J ./gen
-printf "%s\\n" "Generating R.java files: DONE"
+aapt package -f --generate-dependencies --min-sdk-version "$MSDKVERSION" --target-sdk-version "$TSDKVERSION" --version-code "$NOW" --version-name "$PKGNAME" -c "$PSYSLOCALE" --non-constant-id -m -M AndroidManifest.xml -S ./res -J ./gen
 printf "\\e[1;38;5;148m%s;  \\e[1;38;5;114m%s\\n\\e[0m" "aapt: done" "ecj: begun..."
 ecj $ECJENT -d ./obj -sourcepath . $(find "$JDR" -type f -name "*.java") || _PRINTSGE_ ecj
 printf "\\e[1;38;5;149m%s;  \\e[1;38;5;113m%s\\n\\e[0m" "ecj: done" "dx: started..."
