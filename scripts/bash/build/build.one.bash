b4b5ed71c186da5fcb7c73a659eb3c93b3f2ffc3
SDRausty <27742457+SDRausty@users.noreply.github.com>
2021-09-12 11:41:26 -0400

    commit 173601518 on 20210912

--- scripts/bash/build/build.one.bash
+++ scripts/bash/build/build.one.bash
@@ -154,6 +154,7 @@ aapt package -f --min-sdk-version "$MSDKVERSION" --target-sdk-version "$TSDKVERS
 cd output
 ISDOSO="$(head -n 1 "$RDR/.conf/DOSO")"
 [[ $ISDOSO = 0 ]] && (. "$RDR"/scripts/bash/shlibs/buildAPKs/doso.bash || printf "\\e[1;48;5;166m%s\\e[0m\\n" "Signal generated doso.bash ${0##*/} build.one.bash: Continuing...")
+[[ "$ISDOSO" = 0 ]] && (. "$RDR"/scripts/bash/shlibs/buildAPKs/native.bash || printf "\\e[1;48;5;166m%s\\e[0m\\n" "Signal generated native.bash ${0##*/} build.one.bash: Continuing...")
 [[ $ISDOSO = 1 ]] && printf "%s\\n" "To build and include \`*.so\` files in the APK build change the 1 in file ~/${RDR##*/}/.conf/DOSO to a 0.  The command \`build.native.bash\` builds native APKs on device."
 printf "\\e[1;38;5;113m%s\\e[1;38;5;107m\\n" "Adding classes.dex $(find lib -type f -name "*.so") to $PKGNAME.apk..."
 aapt add -v -f "$PKGNAME.apk" classes.dex $(find lib -type f -name "*.so")
