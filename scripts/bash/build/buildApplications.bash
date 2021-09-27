fc3ecc794fb7a6179237a6d25e4f0d9c104a3678
SDRausty <27742457+SDRausty@users.noreply.github.com>
2021-09-27 06:47:48 -0400

    commit 881030712 on 20210927

--- scripts/bash/build/buildApplications.bash
+++ scripts/bash/build/buildApplications.bash
@@ -6,7 +6,7 @@ set -Eeuo pipefail
 shopt -s nullglob globstar
 export RDR="$HOME/buildAPKs"
 . "$RDR/scripts/bash/init/atrap.bash" 67 68 69 "${0##*/} buildApplications.bash"
-export JAD=github.com/BuildAPKs/buildAPKsApps
+export JAD=github.com/SDRausty/buildAPKsApps
 export JID=applications	# job id/name
 . "$HOME/buildAPKs/scripts/bash/init/init.bash" "$@"
 # buildApplications.bash EOF
