0e49fbcb9184b7dba7ada5e9fe95cf4a1982abc6
SDRausty <27742457+SDRausty@users.noreply.github.com>
2022-02-22 02:38:14 -0500

    commit 155001798 on 20220222

--- scripts/bash/init/ushlibs.bash
+++ scripts/bash/init/ushlibs.bash
@@ -17,15 +17,15 @@ _IRGR_() { # initialize a remote git repository
 _UFSHLIBS_() { # add and update submodules
 	declare -A ARSHLIBS # declare associative array for available submodules
 	ARSHLIBS=([scripts/bash/shlibs]="shlibs/shlibs.bash" [scripts/sh/shlibs]="shlibs/shlibs.sh" [opt/api/github]="BuildAPKs/buildAPKs.github" [opt/db]="BuildAPKs/db.BuildAPKs")
-	_ADDMODULE_() { 
-		printf "\\e[1;7;38;5;96mUpdating submodule ~/%s from Internet site %s address:\\e[0m\\n" "${RDR##*/}/$MLOC" "$SIAD/${ARSHLIBS[$MLOC]}" ; git submodule add "$SIAD/${ARSHLIBS[$MLOC]}" "$MLOC" || printf "Cannot update module ~/%s/scripts/%s: Continuing...\\n" "${RDR##*/}" "$MLOC"
+	_ADDMODULE_() {
+		printf "\\e[1;7;38;5;96mAdding submodule ~/%s from Internet site %s address:\\e[0m\\n" "${RDR##*/}/$MLOC" "$SIAD/${ARSHLIBS[$MLOC]}" ; git submodule add "$SIAD/${ARSHLIBS[$MLOC]}" "$MLOC" || printf "Cannot add module ~/%s/scripts/%s: Continuing...\\n" "${RDR##*/}" "$MLOC"
 		_SLEEPSHUF_
 	}
-	_UPDATEMODULE_() { 
+	_UPDATEMODULE_() {
 		printf "\\e[1;7;38;5;96mUpdating submodule ~/%s from Internet site %s address:\\e[0m\\n" "${RDR##*/}/$MLOC" "$SIAD/${ARSHLIBS[$MLOC]}" ; git submodule update --init --depth 1 --recursive --remote "$MLOC" || printf "Cannot update module ~/%s/scripts/%s: Continuing...\\n" "${RDR##*/}" "$MLOC"
 		_SLEEPSHUF_
 	}
-	_SLEEPSHUF_() { 
+	_SLEEPSHUF_() {
 		sleep 0.$(shuf -i 24-72 -n 1) # add network latency support on fast networks
 	}
 	printf "\\e[1;7;38;5;98mInstalling %s prerequisite components of ~/%s/:\\e[0m\\n" "${#ARSHLIBS[@]}" "${RDR##*/}"
